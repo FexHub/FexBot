f1a8c105dc4b4e71a77af4f87d7346581bc9f9f4
Haloger7 <korolgoda2017@gmail.com>
2022-05-03 12:02:23 +0300

    Uhu

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+
+client.login("OTcwOTY4NzczMjMyMzEyMzIw.YnDrIw.6u7vMwrb2tzY9qC-3BGE03nwOZA");
