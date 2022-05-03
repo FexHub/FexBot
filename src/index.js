0caad386eb1a045d10aea071f74bfc3b5ca953b2
Haloger7 <korolgoda2017@gmail.com>
2022-05-03 21:54:32 +0300

    Update to CJS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const Discord = require('discord.js');
+// Import modules
+import Discord from 'discord.js'
 
+// Create new Client
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once("ready", async () => {
     console.log(`\x1b[38;5;49m[✅  | Info] | We're glad to see you, ${client.user.username}!\x1b[0m`);
 })
 
-client.login("OTcwOTY4NzczMjMyMzEyMzIw.YnDrIw.jZkgKJK3-EAhesF1Ev4QBoOQ8_w");
+client.login("OTcwOTY4NzczMjMyMzEyMzIw.YnDrIw.8uRBVLx4UTT2O_1UGJ2RHLJAoNo");
