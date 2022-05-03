ba8e5b49abd3ee0f974c58117fa44bc9a32353ba
Haloger7 <korolgoda2017@gmail.com>
2022-05-03 22:26:28 +0300

    Deploy

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import Discord from 'discord.js'
 // Create new Client
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 
+// Ready event
 client.once("ready", async () => {
     console.log(`\x1b[38;5;49m[✅  | Info] | We're glad to see you, ${client.user.username}!\x1b[0m`);
 })
 
-client.login("OTcwOTY4NzczMjMyMzEyMzIw.YnDrIw.8uRBVLx4UTT2O_1UGJ2RHLJAoNo");
+// Login to bot account
+client.login("");
